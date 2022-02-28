eda507ef34015357f8c923c0f42e8b0cb51fcf61
GitHub Action <action@github.com>
2022-02-28 06:20:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/evenljj/evenideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/evenljj/evenideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.evenljj.evenideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.evenljj.evenideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/evenljj/evenideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/evenljj/evenideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.evenljj.evenideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.evenljj.evenideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
